d503df5a6a43d00bd4409a4ee4ab02e1901dd4ce
Your GitHub Namerakhi103103 <rakhi103103@users.noreply.github.com>
2026-04-08 10:57:47 +0530

    done Normalization,find unique value with count,rw mean and top 3 larget number in array in numpy

--- numpy/pratice.py
+++ numpy/pratice.py
@@ -17,6 +17,40 @@ print(matrix)
 arr = np.array([10,20,30,40,50])
 arr[arr>30]=-1
 print(arr)
+
+# Q---------------Q10 – Normalize Array-------------------------------------
+arr1 =np.array([10,20,30,40,50])
+normalization = (arr1-arr1.min())/(arr1.max()-arr1.min())
+print(f"The normalization of {arr1} is {normalization}")
+
+
+# Q-----------Q11 – Find Unique Values with Count------------------------
+
+arr2 = int(input("Enter the number of the array: "))
+numm = []
+for i in range(arr2):
+    nu = int(input("Enter the name to check unique value: "))
+    numm.append(nu)
+
+arra = np.array(numm)
+values,counts =np.unique(arra,return_counts=True)
+
+for v,c in zip(values,counts):
+    print(v,":",c)
+
+
+# Q------------------Q12 – Row Mean----------------------------------
+mat = np.array([[1,2,3],
+                [4,5,6],
+                [7,8,9]])
+row_mean = np.mean(mat,axis=1)
+print(f"The mean for reavh row for the givn matrix {mat} is {row_mean}")
+
+# Q----------------Q13 – Find Top 3 Largest Numbers-----------------------
+ar = np.array([12,45,2,41,31,10])
+top3 = np.partition(ar,-3)[-3:][::-1]
+print(f"the top 3 max numbers in the given array {ar} is {top3}")
+
     
 
 
